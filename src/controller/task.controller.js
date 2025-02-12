6870b6cc92446179f22c6a5a810ad99d4d8e5ca8
Jorge Tomé <jorge.tomejr@outlook.com>
2025-02-12 16:30:44 -0400

    feat: move delete task endpoint to a controller

--- src/controller/task.controller.js
+++ src/controller/task.controller.js
@@ -43,6 +43,49 @@ class TaskController {
             this.res.status(500).send(error.message);
         }
     }
+
+    async updateTask(){
+        try {
+            const taskId = this.req.params.id;
+            const taskToUpdate = await TaskModel.findById(taskId);
+    
+            const allowedUpdates = ["isCompleted"];
+            const requestUpdates = Object.keys(this.req.body);
+    
+            for(const update of requestUpdates){
+                if(allowedUpdates.includes(update)){
+                    taskToUpdate[update] = this.req.body[update];
+                }else{
+                    return this.res.status(500).send("problema na edição")
+                }
+            }
+    
+            await taskToUpdate.save();
+            return this.res.status(200).send(taskToUpdate);
+            
+        } catch (error) {
+            return this.res.status(500).send(error.message);
+        }
+    }
+
+    async deleteTask(){
+        const { id } = this.req.params
+
+        const validatedId = mongoose.isValidObjectId(id);
+        if(!validatedId){
+            return this.deleteTaskres.status(400).send("Id inválido");
+        }
+    
+        try{
+            const taskdeleted =  await TaskModel.findByIdAndDelete(id);
+            if(!taskdeleted){
+                return this.res.status(400).send("tarefa não encontrada");
+            }
+            this.res.status(200).send("Tarefa deletada com sucesso")
+        }catch(erro){
+            this.res.status(500).send("Error ao deletar tarefa");
+        }
+    }
 }
 
 export default TaskController;
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -19,47 +19,11 @@ router.post("/tasks", async (req, res) => {
 })
 
 router.patch("/tasks/:id", async (req, res) => {
-    try {
-        const taskId = req.params.id;
-        const taskToUpdate = await TaskModel.findById(taskId);
-
-        const allowedUpdates = ["isCompleted"];
-        const requestUpdates = Object.keys(req.body);
-
-        for(const update of requestUpdates){
-            if(allowedUpdates.includes(update)){
-                taskToUpdate[update] = req.body[update];
-            }else{
-                return res.status(500).send("problema na edição")
-            }
-        }
-
-        await taskToUpdate.save();
-        return res.status(200).send(taskToUpdate);
-        
-    } catch (error) {
-        return res.status(500).send(error.message);
-    }
+    return new TaskController(req, res).updateTask();
 })
 
 router.delete("/tasks/:id", async (req, res) => {
-    const { id } = req.params
-
-    const validatedId = mongoose.isValidObjectId(id);
-    if(!validatedId){
-        return res.status(400).send("Id inválido");
-    }
-
-    try{
-        const taskdeleted =  await TaskModel.findByIdAndDelete(id);
-        if(!taskdeleted){
-            return res.status(400).send("tarefa não encontrada");
-        }
-        res.status(200).send("Tarefa deletada com sucesso")
-    }catch(erro){
-        res.status(500).send("Error ao deletar tarefa");
-    }
-
+    return new TaskController(req, res).deleteTask();
 })
 
 export default router;
