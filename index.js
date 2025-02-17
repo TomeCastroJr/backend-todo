5f4396eaf48f7ba8b0768f2453eebd5329931125
Jorge Tomé <jorge.tomejr@outlook.com>
2025-02-17 17:53:13 -0400

    feat: add cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from"cors";
 
 import connectToDatabase from"./src/mongoose/mongoose.database.js";
 import taskRouter from "./src/routes/tasks.routes.js";
 
 dotenv.config();
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use("/",taskRouter);
